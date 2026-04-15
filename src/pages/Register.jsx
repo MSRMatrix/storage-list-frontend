ba8bbea468ffd7ec5cf834875991ab1099e8c0df
TwinMine <michaelrolo11@gmail.com>
2026-04-15 16:46:49 +0200

    add Register form and function

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,35 @@
 const Register = () => {
-    return(
-        <>
-    Register 
-        </>
-        
-    )
-}
+
+    function register(e){
+        e.preventDefault()
+        const formData = new FormData(e.target);
+    const formDataObject = {};
+    formData.forEach((value, key) => {
+        formDataObject[key] = value;
+    })
+    }
+
+  const registerForm = [
+    {
+      name: "Name",
+    },
+    { name: "Email" },
+    { name: "Repeat Email" },
+    { name: "Password" },
+  ];
+  return (
+    <>
+      <form action="" onSubmit={(e) => register(e)}>
+        {registerForm.map((item) => {
+          <fieldset key={item.name}>
+            <legend>{item.name}</legend>
+            <input name={item.name} type="text" />
+          </fieldset>;
+        })}
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
+};
 
 export default Register;
