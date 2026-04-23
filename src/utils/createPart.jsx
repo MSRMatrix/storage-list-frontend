4abacd15838e2ffbe6951294bb4541e9fa010bbe
TwinMine <michaelrolo11@gmail.com>
2026-04-23 07:24:58 +0200

    add backend createPart

--- src/utils/createPart.jsx
+++ src/utils/createPart.jsx
@@ -1,54 +1,47 @@
 import { activateMessage } from "./messageFunctions";
 
-
-
 const URL = import.meta.env.VITE_BACKENDURL;
-  async function test(){
-     try {
-      const response = await fetch(
-        `${URL}/part/create`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-        }
-      );
-      const data = await response.json();
-      if (!response.ok) {
-        return console.log(data);
-      } else {
-        console.log(data);
-      }
-    } catch (error) {
-      console.error("Error toggling 2FA:", error);
+async function test(newItem) {
+  try {
+    const response = await fetch(`${URL}/part/create`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(newItem),
+    });
+    const data = await response.json();
+    if (!response.ok) {
+      return console.log(data);
+    } else {
+      console.log(data);
     }
+  } catch (error) {
+    console.error("Error toggling 2FA:", error);
   }
+}
 
-export function createPart(e, partsContext, setPartsContext, setMessageContext) {
+export function createPart(
+  e,
+  partsContext,
+  setPartsContext,
+  setMessageContext,
+) {
   e.preventDefault();
-  const topic = "Teile Status";  
 
+  const topic = "Teile Status";
   const formData = new FormData(e.target);
 
   const newItem = {
-    id: crypto.randomUUID(),
     partNumber: formData.get("partNumber"),
     name: formData.get("name"),
     quantity: Number(formData.get("quantity")),
     price: Number(formData.get("price")),
     lowLimit: Number(formData.get("lowLimit")),
     description: formData.get("description") || "No description",
-    deleted: false
+    deleted: false,
   };
-
-  console.log(newItem);
-  
-  function updateParts(newArray) {
-    setPartsContext(newArray);
-    localStorage.setItem("parts", JSON.stringify(newArray));
-  }
 
   const exists = partsContext.some(
     (item) => item.partNumber === newItem.partNumber
@@ -57,22 +50,39 @@ export function createPart(e, partsContext, setPartsContext, setMessageContext) 
   let updated;
 
   if (exists) {
+    const question = confirm(
+      "Part-number exists already! Add quantity to existing part?"
+    );
+
+    if (!question) {
+      alert("Creating Part stopped!");
+      return;
+    }
+
     updated = partsContext.map((item) => {
       if (item.partNumber === newItem.partNumber) {
         return {
           ...item,
-          quantity: item.quantity + newItem.quantity
+          quantity: item.quantity + newItem.quantity,
         };
       }
       return item;
     });
+
   } else {
     updated = [...partsContext, newItem];
   }
 
+  test(newItem);
+
+  function updateParts(newArray) {
+    setPartsContext(newArray);
+    localStorage.setItem("parts", JSON.stringify(newArray));
+  }
+
   updateParts(updated);
+
   const text = "Teil hinzugefügt";
-  const status = "200"
-  activateMessage(topic, text, status, setMessageContext)
-// test()
+  const status = "200";
+  activateMessage(topic, text, status, setMessageContext);
 }
