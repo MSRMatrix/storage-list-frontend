ee935553e147a4de432388ef6e497c7eccdbf526
TwinMine <michaelrolo11@gmail.com>
2026-05-13 16:14:21 +0200

    add comment in logout

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 const Logout = () => {
+
+    const isLogOut = useState(false)
+
+    // Ein Confirm oder ein Fenster dass dann fragt ob man sich ausloggen will
+
+
     return(
         <>
-    Logout 
         </>
         
     )
